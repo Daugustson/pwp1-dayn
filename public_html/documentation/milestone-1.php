aea7e7b64ebcbe82cd0ea93054a925318e9917c9
Dayn Augustson <Daugustson@cnm.edu>
2018-10-16 16:19:30 -0600

    frame work for milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,50 @@
+<DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Milestone 1 for PWP </title>
+		</head>
+		<body>
+			<h1>Milestone 1</h1>
+			<h2>Purpose, Audience, Goal</h2>
+			<ol>
+				<li><strong>Purpose:</strong></li>
+				<li><strong>Audience:</strong></li>
+				<li><strong>Goal:</strong></li>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</ol>
+
+			<h2>Persona</h2>
+			<img src="">
+			<ul>
+				<li><strong>Name:</strong> </li>
+				<li><strong>Age:</strong> </li>
+				<li><strong>Gender:</strong> </li>
+				<li><strong>Ethnicity:</strong> </li>
+				<li><strong>Occupation:</strong> </li>
+				<li><strong>Technology:</strong> </li>
+				<li><strong>Needs, Goals, and Features:</strong></li>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</ul>
+
+			<h2>User Story</h2>
+			<p></p>
+
+
+
+
+
+
+		</body>
+
+		</html>
