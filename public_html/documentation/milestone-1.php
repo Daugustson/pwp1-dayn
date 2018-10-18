762d77e287604021c8853fd8f23d8209e5601cbc
Dayn Augustson <Daugustson@cnm.edu>
2018-10-17 19:05:12 -0600

    ADDED TXT TO FRAMWORK

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,19 +6,13 @@
 		</head>
 		<body>
 			<h1>Milestone 1</h1>
+
 			<h2>Purpose, Audience, Goal</h2>
-			<ol>
-				<li><strong>Purpose:</strong></li>
-				<li><strong>Audience:</strong></li>
-				<li><strong>Goal:</strong></li>
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
-			</ol>
+
+				<P><strong>Purpose:</strong> The purpose of this website is to display my skills as a full stack web developer.</P>
+			   <P><strong>Audience:</strong>This website is geared towards people that are starting a business and need a website or have an establish business and want to do a makeover of their web site to spice things up. </P>
+				<p><strong>Goal:</strong>The primary goal of this website is to draw clients in and create business contacts/opportunities.</p>
+
 
 			<h2>Persona</h2>
 			<img src="">
@@ -28,17 +22,25 @@
 				<li><strong>Gender:</strong> Female</li>
 				<li><strong>Ethnicity:</strong> White</li>
 				<li><strong>Occupation:</strong> Brew Pub Owner</li>
-				<li><strong>Technology:</strong>She has  a strong grasp of evey day technologies</li>
-				<li><strong>Needs, Goals, and Features:</strong></li>
-				<ul>
-					<li>Jody needs a dynamic website to show case her new brew pub opening in a couple of months.</li>
-					<li></li>
-					<li></li>
-				</ul>
+				<li><strong>Technology:</strong>She has a strong grasp of evey day technologies.</li>
 			</ul>
 
-			<h2>User Story</h2>
-			<p></p>
+			<h3>Needs, Goals, Features and Frustrations</h3>
+				<p>Jody needs a dynamic website to showcase her new brew pub opening in a couple of months.
+					She has always loved Ireland and want to bring a little piece of that to Durango Colorado.
+					Everything from Ireland style beer, food, culture, and decor has been meticulously vetted by her.
+					She really needs a website that exhibits her passion for Ireland culture.
+					She has a hard and fast deadline but wants the website to keep people up to date with what beer is on tap,
+					menu items, specials, music, game nights, and wants an interactive loyalty club.
+					Her biggest frustration is the inability to find someone as meticulous and passionate as she is.
+					Jody admits she is a hard taskmaster, but that is only because she wants the best. </p>
+
+			<h3>User Story</h3>
+			   <P>Jody wants and needs a website that is not only aesthetically pleasing, but also flexible enough to allow her
+				to make weekly/daily changes to items (beers on tap, food specials, etc..) on her website without calling the
+				website developer.</P>
+			<br/>
+			<br/>
 
 
 
