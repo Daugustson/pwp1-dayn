cfeca1aabb407caa9c583ad57674da7fa0430105
Dayn Augustson <daugustson@cnm.edu>
2018-10-31 17:07:06 -0600

    set frame work for PWP Milestone 2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,10 +1,10 @@
-
-		<body><DOCTYPE html>
-				<html lang="en">
-					<head>
-						<meta charset="UTF-8">
-						<title>Milestone 2 for PWP </title>
-					</head>
+<DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Milestone 2 for PWP </title>
+		</head>
+		<body>
 			<h1>Milestone 2b</h1>
 
 			<h2>Welcome</h2>
@@ -18,3 +18,4 @@
 
 			<h2>Contact</h2>
 			<p></p>
+		</body>
