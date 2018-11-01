b18b4a2c51871cdd7b8ba69f625125ec57466ae3
Dayn Augustson <daugustson@cnm.edu>
2018-10-31 22:09:12 -0600

    added second pic and text

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,17 +8,26 @@
 			<h1>Milestone 2b</h1>
 
 			<h2>Welcome</h2>
-			<p></p>
+			<p>This is where I will introduce myself along with my professional tittle.  This will all be laid on back drop of the Sandia mountain at sunset. </p>
 
 			<h2>About Me</h2>
-			<p></p>
+			<p>In this section I will show my newly found professional aptitudes. Listing the skill sets I have acquired during my time at Deep Dive Bootcamp.  I hope to convey my willingness to go the extra mile and be a team player in this segment. Will add a picture of me to give a little personal touch. </p>
+
+			<h2>Testimonials</h2>
+			<p>My testimonial spot is where I hope to get my capstone project team mates a chance to give a little insight to what it was like to work with me. Looking at doing a simple click slider to toggle through the testimonials.</p>
 
 			<h2>Portfolio</h2>
-			<p></p>
+			<p>I will highlight my capstone project here. This section will grow over time as I complete more projects. </p>
 
 			<h2>Contact</h2>
-			<p></p>
+			<p>This will be a simple contact form, github, linkedin, email, and resume section.</p>
 
 			<h2>Moble Site</h2>
 			<img src="Moble.png" alt="Moble Site">
+
+			<h2>Desktip Site</h2>
+			<img src="Desktop.png" alt="Desktop Site">
+			<br>
+			<br>
 		</body>
+	</html>
