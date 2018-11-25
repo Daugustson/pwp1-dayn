78a60b674c27217385f686a55aad76baba2d33a4
Dayn Augustson <daugustson@cnm.edu>
2018-11-25 11:39:47 -0700

    commented out testimonials

--- public_html/index.php
+++ public_html/index.php
@@ -76,46 +76,46 @@
 		</section>
 
 		<!-- This is the Testimonials-->
-			<section class="py-3 color2">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col">
-							<h2 class="text-center front-weight-bold">Testimonials</h2>
-							<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-								<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-									<ol class="carousel-indicators">
-										<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-										<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-										<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-									</ol>
-									<div class="carousel-inner">
-										<div class="carousel-item active">
-											<img class="d-block w-100" src="frist" alt="First slide">
-										</div>
-										<div class="carousel-item">
-											<img class="d-block w-100" src="second" alt="Second slide">
-										</div>
-										<div class="carousel-item">
-											<img class="d-block w-100" src="third" alt="Third slide">
-										</div>
+		<!--<section class="py-3 color2">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col">
+						<h2 class="text-center front-weight-bold">Testimonials</h2>
+						<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+								<ol class="carousel-indicators">
+									<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+								</ol>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img class="d-block w-100" src="frist" alt="First slide">
 									</div>
-									<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-										<span class="sr-only">Previous</span>
-									</a>
-									<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-										<span class="carousel-control-next-icon" aria-hidden="true"></span>
-										<span class="sr-only">Next</span>
-									</a>
+									<div class="carousel-item">
+										<img class="d-block w-100" src="second" alt="Second slide">
+									</div>
+									<div class="carousel-item">
+										<img class="d-block w-100" src="third" alt="Third slide">
+									</div>
 								</div>
+								<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
 							</div>
 						</div>
 					</div>
 				</div>
-			</section>
+			</div>
+		</section>-->
 
-			<!--This is Profile section-->
-				<section class="py-3 color1">
+		<!--This is Profile section-->
+				<section class="py-3 color2">
 					<h2 class="text-left front-weight-bold">Profile</h2>
 
 					<div class="container-fluid">
@@ -200,6 +200,7 @@
 
 
 			</div>
+			<hr>
 
 
 		</form>
