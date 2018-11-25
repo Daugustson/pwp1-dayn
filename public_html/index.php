215adc8b27b1e775e2453931fe6b329cef176490
Dayn Augustson <daugustson@cnm.edu>
2018-11-25 10:48:47 -0700

    comited out the nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -10,7 +10,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
 		<!-- custom CSS stylesheet -->
-		<link rel="stylesheet" href="../../css/style.css" type="text/css" />
+		<link rel="stylesheet" href="../css/style.css" type="text/css" />
 
 		<!-- LINK YOUR CSS FILES DOWN HERE -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -32,7 +32,7 @@
 
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
+					<!--<li class="nav-item">
 						<a class="nav-link" href="#">About</a>
 					<li class="nav-item">
 						<a class="nav-link" href="#">Testimonials</a>
@@ -41,7 +41,7 @@
 					<li class="nav-item">
 						<a class="nav-link" href="#">Contact</a>
 					</li>
-				</ul>
+				</ul>-->
 			</div>
 		</nav>
 		</header>
@@ -65,7 +65,7 @@
 			<div class="container">
 				<div  class="row">
 					<div class="col-sm-3">
-						<img class="img-fluid rounded-circle width " src="dayn.jpg" alt="responsive image">
+						<img class="img-fluid rounded-circle width " src="documentation/dayn.jpg" alt="responsive image">
 					</div>
 					<div class="col-sm-9">
 						<h2 class="text-center front-weight-bold">About me</h2>
