4646aab54d26973c74dd20453b126a2db402056b
Dayn Augustson <daugustson@cnm.edu>
2018-11-26 18:45:37 -0700

    adding reCAPTCHA

--- public_html/index.php
+++ public_html/index.php
@@ -28,6 +28,7 @@
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<script src='https://www.google.com/recaptcha/api.js?render=6LfUQn0UAAAAAM39ERrMNIwVLJTOReivc_BxXNz8'></script>
 
 
 
